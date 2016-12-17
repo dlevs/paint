4eb112d588a91f36babb07b4da52966c7294b5d0
Daniel <dlevett@hotmail.co.uk>
2016-12-17 23:37:26 +0000

    add previously .gitignored App.js

---
+++ components/App/App.js
@@ -0,0 +1,10 @@
+import React, { PropTypes } from 'react';
+import Toolbar from '../Toolbar';
+import Canvas from '../Canvas';
+
+export default () => (
+	<main>
+		<Toolbar />
+		<Canvas />
+	</main>
+);
